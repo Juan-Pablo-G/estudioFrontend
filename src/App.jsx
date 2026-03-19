039fc55af29c5ef1349b92366a7300e3967b0a1c
Juan <juan@MacBook-Pro-de-Juan.local>
2026-03-19 11:06:24 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react'
 
-const API_URL = 'https://estudiobackend.onrender.com/api/posts'
+const API_BASE= 'https://estudiobackend.onrender.com/api/posts'
 
 //const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
